48b18e3bf8a81d28d09877ee970ca5849072da09
James Hepworth <james@opusmeum.dev>
2021-05-12 08:40:12 +0200

    Commit 1

---
+++ test/src/main.c
@@ -0,0 +1,30 @@
+#define STM32F051
+#include <stdint.h>
+#include "stm32f0xx.h"
+
+void init_leds();
+
+void main()
+{
+	init_leds();
+	//init_buttons();
+
+	while(1)
+	{
+		// Switch GPIOB pins 0-7 on
+		GPIOB->ODR = 0xFF;
+	}
+}
+
+void init_leds()
+{
+	// Enable GPIOB clock
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+
+
+	// Setup GPIOB pins 0-7 as outputs
+	GPIOB->MODER |= GPIO_MODER_MODER0_0 | GPIO_MODER_MODER1_0 | GPIO_MODER_MODER2_0 | GPIO_MODER_MODER3_0;
+	GPIOB->MODER |= GPIO_MODER_MODER4_0 | GPIO_MODER_MODER5_0 | GPIO_MODER_MODER6_0 | GPIO_MODER_MODER7_0;
+
+
+}
